367b59925fe4aba6fde0984bad3be5bf06880fa5
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2024-06-21 17:51:54 +0100

    tidying + cta

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ document.addEventListener('DOMContentLoaded', () => {
   // Initial check
   handleScroll();
 });
+
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+        const ctaSection = document.querySelector('.cta');
+        const ctaLinks = document.querySelectorAll('.cta-links');
+
+        ctaSection.addEventListener('mouseenter', () => {
+            ctaLinks.forEach(link => link.classList.add('cta-links-bold'));
+        });
+
+        ctaSection.addEventListener('mouseleave', () => {
+            ctaLinks.forEach(link => link.classList.remove('cta-links-bold'));
+        });
+    });
